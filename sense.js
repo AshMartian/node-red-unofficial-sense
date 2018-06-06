0a4e5e86d5ca781aa4ae7f753fb57ce1ff239c53
Brandon Martin <brandonmartin@brandons-mbp.lan>
2018-06-05 19:29:08 -0700

    Initial commit

---
+++ sense.js
@@ -0,0 +1,75 @@
+module.exports = function(RED) {
+    "use strict";
+    const sense = require('unofficial-sense')
+
+    function SenseConfig(config) {
+        RED.nodes.createNode(this, config);
+        var email = this.credentials.email;
+        var password = this.credentials.password;
+        var globalContext = this.context().global;
+
+        sense({
+            email: email,
+            password: password
+        }, (data) => {
+            globalContext.set('sense-realtime', data);
+            this.realtime = data;
+        }).then(senseObj => {
+            senseObj.getDevices().then(devices => {
+                globalContext.set('sense-devices', devices.data);
+                this.senseDevices = devices.data;
+            })
+            this.senseObj = senseObj;
+        })
+    }
+    function senseUpdate(config) {
+        RED.nodes.createNode(config);
+        var node = this;
+        var globalContext = this.context().global;
+        this.senseConfig = RED.nodes.getNode(this.config);
+        if(this.senseConfig && this.senseConfig.senseObj) {
+            this.senseConfig.senseObj.events.on('data', (data) => {
+                this.send({
+                    payload: data
+                })
+            });
+        }
+    }
+
+    function SenseDeviceOn(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        var globalContext = this.context().global;
+        this.senseConfig = RED.nodes.getNode(this.config);
+
+        node.on('input', (msg) => {
+            var devices = this.senseConfig.realtime.devices;
+            var msg2 = Object.assign({}, msg);
+            if(devices) {
+                let foundDevice = devices.filter((device) => {
+                    return device.name === msg.payload || device.id === msg.payload
+                })
+                if(foundDevice[0]) {
+                    msg.payload = foundDevice[0]
+                    msg2 = null;
+                } else {
+                    msg = null;
+                    msg2 = {"status": "Device off"}
+                }
+            } else {
+                msg2.payload = {"error": "no devices"}
+                msg = null;
+            }
+            node.send([msg, msg2]);
+        });
+    }
+    
+    RED.nodes.registerType("sense-config", SenseConfig, {
+        credentials: {
+            email: {type:"text"},
+            password: {type:"password"}
+        }
+    });
+    RED.nodes.registerType("sense-update", senseUpdate);
+    RED.nodes.registerType("sense-device-on", SenseDeviceOn);
+}
